c3466a36d7f17976f3719d75c119c276e1d27cb1
TeHbBoMpake <TeHbBoMpake@gmail.com>
2024-08-17 17:47:13 +0300

    Костыль

--- build.sh
+++ build.sh
@@ -31,9 +31,7 @@ mkdir -p "$WINEPREFIX/drive_c/Games/World of Sea Battle/default"
 7z x -y $DOWNLOAD_DIR/launcher.exe -o"$WINEPREFIX/drive_c/Games/Wosb Launcher"
 # 7z x -y $DOWNLOAD_DIR/game_archive.zip -o"$WINEPREFIX/drive_c/Games/World of Sea Battle/default/game"
 
-# Выведем, что у нас в AppImage
-find Wosb.AppDir/usr -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
-
+sleep 10
 sync
 chmod +x $DOWNLOAD_DIR/appimagetool.AppImage
 ARCH=x86_64 $DOWNLOAD_DIR/appimagetool.AppImage Wosb.AppDir/
