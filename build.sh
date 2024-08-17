4453402608e990c545d6b15158d0dfebae04b50d
TeHbBoMpake <TeHbBoMpake@gmail.com>
2024-08-17 15:05:48 +0300

    Init project

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+DOWNLOAD_DIR="./downloads"
+
+# Добавляем wine в AppImage
+mv $DOWNLOAD_DIR/wine-staging.AppImage ./Wosb.AppDir/usr/bin/wine
+chmod +x ./Wosb.AppDir/usr/bin/wine
+ln -sf ./wine ./Wosb.AppDir/usr/bin/winetricks
+# Добавляем overlayfs в AppImage
+mv $DOWNLOAD_DIR/fuse-overlayfs ./Wosb.AppDir/usr/bin/overlayfs
+chmod +x ./Wosb.AppDir/usr/bin/overlayfs
+
+# Создаем префикс
+export WINEPREFIX=$PWD/Wosb.AppDir/app/wine 
+
+WINE=$PWD/Wosb.AppDir/usr/bin/wine
+WINETRICKS=$PWD/Wosb.AppDir/usr/bin/winetricks
+
+rm -rf "$WINEPREFIX"
+
+$WINETRICKS -q --force dxvk dotnetdesktop7
+
+$WINE reg ADD "HKCU\\Software\\Xsolla\\3365\\204981\\default" /f /v "isInstalled" /t REG_SZ /d "true"
+$WINE reg ADD "HKCU\\Software\\Xsolla\\3365\\204981\\default" /f /v "prefix" /t REG_SZ /d "C:/Games/World of Sea Battle"
+
+# Добавляем лаунчер и сам возб в префикс
+mkdir -p "$WINEPREFIX/drive_c/Games/World of Sea Battle/default"
+7z x -y $DOWNLOAD_DIR/launcher.exe -o"$WINEPREFIX/drive_c/Games/Wosb Launcher"
+# 7z x -y $DOWNLOAD_DIR/game_archive.zip -o"$WINEPREFIX/drive_c/Games/World of Sea Battle/default/game"
+
+
+sync
+chmod +x $DOWNLOAD_DIR/appimagetool.AppImage
+ARCH=x86_64 $DOWNLOAD_DIR/appimagetool.AppImage Wosb.AppDir/
---
+++ fetch.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+set -e
+
+# URL страницы с загрузками игры
+PAGE_URL="https://www.worldofseabattle.com/download2"
+
+# API URL для релизов Wine-staging и fuse-overlayfs
+WINE_API_URL="https://api.github.com/repos/mmtrt/WINE_AppImage/releases"
+FUSE_OVERLAYFS_API_URL="https://api.github.com/repos/containers/fuse-overlayfs/releases"
+
+#Appimagetool URL
+APPIMAGETOOL_URL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage"
+
+# Путь для сохранения файлов
+DOWNLOAD_DIR="./downloads"
+mkdir -p "$DOWNLOAD_DIR"
+
+# Скачиваем страницу с игры
+echo "Загрузка страницы..."
+PAGE_CONTENT=$(curl -s "$PAGE_URL")
+
+# Извлечение ссылок на лаунчер и архив
+LAUNCHER_URL=$(echo "$PAGE_CONTENT" | grep -oP '(?<=href=")[^"]*installer\.launcher\.xsolla\.com[^"]*')
+# ARCHIVE_URL=$(echo "$PAGE_CONTENT" | grep -oP '(?<=href=")[^"]*drive\.usercontent\.google\.com[^"]*')
+
+# Проверка на наличие ссылок
+if [ -z "$LAUNCHER_URL" ]; then #if [ -z "$LAUNCHER_URL" ] || [ -z "$ARCHIVE_URL" ]; then
+  echo "Не удалось найти ссылки для загрузки. Проверьте URL страницы."
+  exit 1
+fi
+
+# Скачивание лаунчера
+echo "Скачивание лаунчера..."
+curl -L -o "$DOWNLOAD_DIR/launcher.exe" "$LAUNCHER_URL"
+
+# Скачивание архива
+# echo "Скачивание архива..."
+# curl -L -o "$DOWNLOAD_DIR/game_archive.zip" "$ARCHIVE_URL&confirm=t"
+
+# Получаем информацию о последнем релизе Wine-staging
+echo "Получение информации о последнем релизе Wine-staging..."
+WINE_DOWNLOAD_URL=$(curl -s "$WINE_API_URL" | grep -oP '(?<="browser_download_url": ")[^"]*' | grep -vE 'ge_proton' | grep 'continuous-staging' | head -n 1)
+
+# Проверка, была ли найдена ссылка на Wine-staging
+if [ -z "$WINE_DOWNLOAD_URL" ]; then
+  echo "Не удалось найти ссылку на последний релиз Wine-staging."
+  exit 1
+fi
+
+# Скачивание последней версии Wine-staging
+echo "Скачивание последней версии Wine-staging..."
+curl -L -o "$DOWNLOAD_DIR/wine-staging.AppImage" "$WINE_DOWNLOAD_URL"
+
+# Получаем информацию о последнем релизе fuse-overlayfs
+echo "Получение информации о последнем релизе fuse-overlayfs..."
+FUSE_OVERLAYFS_DOWNLOAD_URL=$(curl -s "$FUSE_OVERLAYFS_API_URL" | grep -oP '(?<="browser_download_url": ")[^"]*' | grep x86_64 | head -n 1)
+
+# Проверка, была ли найдена ссылка на fuse-overlayfs
+if [ -z "$FUSE_OVERLAYFS_DOWNLOAD_URL" ]; then
+  echo "Не удалось найти ссылку на последний релиз fuse-overlayfs."
+  exit 1
+fi
+
+# Скачивание последней версии fuse-overlayfs
+echo "Скачивание последней версии fuse-overlayfs..."
+curl -L -o "$DOWNLOAD_DIR/fuse-overlayfs" "$FUSE_OVERLAYFS_DOWNLOAD_URL"
+
+# Скачивание последней версии Appimagetool
+echo "Скачивание последней версии Appimagetool"
+curl -L -o "$DOWNLOAD_DIR/appimagetool.AppImage" "$APPIMAGETOOL_URL"
+
+echo "Все файлы успешно скачаны в $DOWNLOAD_DIR."
